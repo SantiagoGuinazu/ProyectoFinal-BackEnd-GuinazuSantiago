b39b7b89341375f7e2cd74d0acd2149ae1111099
Santiago Guinazu <santigui2003@gmail.com>
2023-10-27 12:23:09 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Proyecto Final
+
+1er Entrega
+
+PUERTO: 127.0.0.1:8080
---
+++ app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import products from "./src/routers/products.js"
+import carts from "./src/routers/carts.js"
+
+const app = express();
+const port = 8080;
+
+app.use(express.json())
+
+app.use("/api/products", products);
+app.use("/api/carts", carts);
+
+app.get("/", function (req, res) {
+    return res.send("Solución laboratorio 3")
+});
+
+app.listen(port, () => {
+    console.log(`Corriendo en el puerto ${port}`);
+});
