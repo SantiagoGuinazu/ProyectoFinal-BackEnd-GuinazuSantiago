646baea217d173c9ef306363b013d9e23f6e703a
Santiago Guinazu <santigui2003@gmail.com>
2023-10-27 20:03:00 -0300

    cambios

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import hbs from "hbs";
 import products from "./src/routers/products.js";
 import carts from "./src/routers/carts.js";
+import initial from "./src/routers/initial.js";
 import __dirname from "./src/utils/utils.js";
 
 const app = express();
@@ -14,18 +15,7 @@ app.use(express.json());
 
 app.use("/api/products", products);
 app.use("/api/carts", carts);
-
-app.get("/products", (req,res) => {
-    return res.render("productos")
-})
-
-app.get("/real-time-products", (req,res) => {
-    return res.render("productos-real-time")
-})
-
-app.get("*", (req,res) => {
-    return res.render("Pagina no encontrada")
-});
+app.use("/", initial);
 
 app.listen(port, () => {
     console.log(`Corriendo en el puerto ${port}`);
--- src/routers/initial.js
+++ src/routers/initial.js
@@ -2,6 +2,16 @@ import { Router } from "express";
 
 const router = Router()
 
+router.get("/products", (req,res) => {
+    return res.render("productos")
+})
 
+router.get("/real-time-products", (req,res) => {
+    return res.render("productos-real-time")
+})
+
+router.get("*", (req,res) => {
+    return res.render("Pagina 404")
+});
 
 export default router;
