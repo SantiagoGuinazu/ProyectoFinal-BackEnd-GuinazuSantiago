8e7f3d1f5984c2fddb5dceab60edca037d056cc2
Santiago Guinazu <santigui2003@gmail.com>
2023-11-08 22:07:59 -0300

    mongoose 2

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ import __dirname from './utils.js';
 import Productos from './models/productos.js';
 import { Server } from 'socket.io';
 
+import UserModel from './models/users.models.js';
+import mongoose, { mongo } from 'mongoose';
+
 const app = express();
 const port = 8080;
 
@@ -18,9 +21,28 @@ app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.json());
 
+app.get('/api/users', async (req, res) => { 
+    const users = await UserModel.find()
+
+    res.json({ status: 'success', payload: users })
+})
+
 app.use('/api/products', products);
 app.use('/api/carts', carts);
 app.use('/', initial);
+
+
+const url = 'mongodb+srv://santigui2003:arquitectura10@santiagocluster.vw1wy4u.mongodb.net/' // Moongose
+mongoose.connect(url, { dbName: 'clase14_555' })
+    .then(() => {
+        console.log("DB connected")
+    })
+    .catch(e => {
+        console.error('Error connecting to DB')
+    })
+
+
+
 
 const httpServer = app.listen(port, () => {
     console.log(`Corriendo en el puerto ${port}`);
@@ -31,7 +53,7 @@ const io = new Server(httpServer);
 io.on('connection', socket => {
     console.log('Nuevo cliente conectado');
 
-    socket.on('disconnect', ()=> {
+    socket.on('disconnect', () => {
         console.log('El cliente se ha desconectado')
     })
 
