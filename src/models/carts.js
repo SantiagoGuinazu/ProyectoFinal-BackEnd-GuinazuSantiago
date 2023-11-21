b5e0a8caf1386f1692d4e20fe60855cec82a6f92
Santiago Guinazu <santigui2003@gmail.com>
2023-11-21 12:07:21 -0300

    cambios

--- src/models/carts.js
+++ src/models/carts.js
@@ -3,9 +3,9 @@ import {Schema , model} from 'mongoose';
 const nameCollection = 'Cart';
 
 const CartSchema = new Schema({
-    _id: false,
     products:[
         {
+            _id: false,
             id:{
                 type:Schema.Types.ObjectId,
                 ref:'Producto'
