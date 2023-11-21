ae3191545ec9f3b58d76017b993783fc682b97b4
Santiago Guinazu <santigui2003@gmail.com>
2023-11-21 11:33:25 -0300

    cambios

--- src/routers/initial.js
+++ src/routers/initial.js
@@ -3,13 +3,17 @@ import { productModel } from '../models/productos.js';
 
 const router = Router();
 
-router.get('/', async (req,res) => {
+router.get('/products', async (req,res) => {
     const productos = productModel.find();
-    return res.render('home', {productos, styles:'styles.css'})
+    return res.render('productos', {productos, styles:'styles.css'})
 })
 
 router.get('/real-time-products', (req,res) => {
     return res.render('productos-real-time')
 })
+
+router.get('*', (req, res) => {
+    return res.render('404');
+});
 
 export default router;
