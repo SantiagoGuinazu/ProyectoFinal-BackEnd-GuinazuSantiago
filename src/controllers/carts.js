8ef824420a5505ffeec090350ffef22a81bb2d81
Santiago Guinazu <santigui2003@gmail.com>
2023-11-27 15:22:46 -0300

    pre entrega 2

--- src/controllers/carts.js
+++ src/controllers/carts.js
@@ -1,27 +1,23 @@
 import { request, response } from 'express';
-import { cartModel } from '../models/carts.js';
-
+import { addProductCartService, createCartService, getCartByIdService } from '../services/carts.js';
 
 export const getCartById = async (req= request, res= response) => {
     try {
         const {cid} = req.params;
-        const carrito = await cartModel.findById(cid)
-
+        const carrito = await getCartByIdService(cid);
         if(carrito)
             return res.json({carrito})
         return res.status(404).json({msg: `el carrito con id ${cid} no existe`})
     } catch (error) {
-        console.log('getCartById ->', error)
         return res.status(500).json({msg:"Hablar con admin"})
     }
 }
 
 export const createCart = async (req= request, res= response) => {
     try {
-        const carrito = await cartModel.create({});
+        const carrito = await createCartService();
         return res.json({msg:'Carrito creado', carrito})
     } catch (error) {
-        console.log('createCart ->', error)
         return res.status(500).json({msg:"Hablar con admin"})
     }
 }
@@ -29,20 +25,14 @@ export const createCart = async (req= request, res= response) => {
 export const addProductCart = async (req= request, res= response) => {
     try {
         const { cid, pid } = req.params;
-        const carrito = await cartModel.findById(cid);
+
+        const carrito = await addProductCartService(cid, pid);
+
         if(!carrito)
             return res.status(404).json({msg:`el carrito con id ${cid} no existe`})
-        const productoInCart = carrito.products.find(p=>p.id.toHexString() === pid);
 
-        if(productoInCart)
-            productoInCart.quantity ++;
-        else
-            carrito.products.push({id:pid, quantity:1})
-        
-        carrito.save();
         return res.json({msg: "carrito actualizado", carrito})
     } catch (error) {
-        console.log('addProductCart ->', error)
         return res.status(500).json({msg:"Hablar con admin"})
     }
 }
