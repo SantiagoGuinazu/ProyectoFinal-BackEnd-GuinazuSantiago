23309df5490bb59d5b69ec2abf905fec33a9828c
Santiago Guinazu <santigui2003@gmail.com>
2023-12-01 11:07:18 -0300

    desafio5

--- src/app.js
+++ src/app.js
@@ -9,9 +9,9 @@ import { dbConnection } from './database/config.js';
 import { messageModel } from './models/messages.js';
 import 'dotenv/config';
 import { addProductService, getProductsService } from './services/products.js';
-//import sessions from './routers/session.js'
-//import session from 'express-session';
-//import MongoStore from 'connect-mongo';
+import sessions from './routers/sessions.js'
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
 
 const app = express();
 const port = process.env.port;
@@ -21,22 +21,22 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.json());
-//app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 
-//app.use(session({
-//    store: MongoStore.create({
-//        mongoUrl: mongoURL,
-//        dbName: mongoDB
-//    }),
-//   secret: 'secret',
-//    resave: true,
-//    saveUninitialized: true
-//}))
+app.use(session({
+    store: MongoStore.create({
+        mongoUrl: process.env.URI_Mongo_DB,
+        dbName: process.env.NAME_DB
+    }),
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}))
 
 app.use('/api/products', products);
 app.use('/api/carts', carts);
-//app.use('/api/session', sessions);
+app.use('/api/session', sessions);
 app.use('/', initial);
 
 await dbConnection();
@@ -49,13 +49,13 @@ const io = new Server(httpServer);
 
 io.on('connection', async (socket) => {
 
-    const {payload} = await getProductsService({limit : 100});
+    const { payload } = await getProductsService({ limit: 100 });
     const productos = payload;
 
     socket.emit('productos', payload);
     socket.on('agregarProducto', async (producto) => {
-        const newProduct = await addProductService({...producto})
-        if(newProduct){
+        const newProduct = await addProductService({ ...producto })
+        if (newProduct) {
             productos.push(newProduct)
             socket.emit('productos', productos)
         }
@@ -65,8 +65,8 @@ io.on('connection', async (socket) => {
     socket.emit('message', messages);
 
     socket.on('message', async (data) => {
-        const newMessage = await messageModel.create({...data});
-        if(newMessage) {
+        const newMessage = await messageModel.create({ ...data });
+        if (newMessage) {
             const messages = await messageModel.find();
             io.emit('messageLogs', messages)
         }
--- src/routers/initial.js
+++ src/routers/initial.js
@@ -4,6 +4,18 @@ import { getCartByIdService } from '../services/carts.js';
 
 const router = Router();
 
+function justPublicWitoutSession(req, res, next) {
+    if(req.session?.user) return res.redirect('/profile')
+    
+    return next()
+}
+
+function auth(req, res, next) {
+    if(req.session?.user) return next()
+
+    res.redirect('/login')
+}
+
 router.get('/products', async (req,res) => {
     const result = await getProductsService({...req.query});
     return res.render('productos', {result})
@@ -17,9 +29,19 @@ router.get('/chat', (req,res) => {
     return res.render('chat', {styles: 'chat.css'})
 });
 
-//router.get('/sessions', (req,res) => {
-//    return res.render('sessions', {styles: 'chat.css'})
-//});
+router.get('/login', justPublicWitoutSession, (req, res) => {
+    return res.render('login', {})
+})
+
+router.get('/register', justPublicWitoutSession, (req, res) => {
+    return res.render('register', {})
+})
+
+router.get('/profile', auth, (req, res) => {
+    const user = req.session.user
+
+    res.render('profile', user)
+})
 
 router.get('/cart/:cid', async(req,res)=>{
     const {cid} = req.params;
