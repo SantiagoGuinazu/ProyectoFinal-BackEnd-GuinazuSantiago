fac10f72d618629dd94f8c2587392a90df9157f4
Santiago Guinazu <santigui2003@gmail.com>
2023-12-04 15:38:57 -0300

    cambios post video

--- src/controllers/initial.js
+++ src/controllers/initial.js
@@ -1,6 +1,7 @@
 import { request, response } from "express";
 import { getProductsService } from "../services/products.js";
 import { getCartByIdService } from "../services/carts.js";
+import { getUserEmail, registerUser } from "../services/user.js";
 
 export const realTimeProductsView = async (req = request, res = response) => {
     return res.render('productos-real-time')
@@ -25,6 +26,37 @@ export const loginGet = async (req = request, res = response) => {
     return res.render('login', {styles:'login.css'})
 }
 
+export const loginPost = async (req = request, res = response) => {
+    const {email, password} = req.body;
+
+    const user = await getUserEmail({email})
+
+    if(user && user.password === password){
+        const userName = `${user.name} ${user.lastName}`;
+        req.session.user = userName;
+        req.session.rol = user.rol;
+        return res.redirect('/')
+    }
+    return res.redirect('/login')
+}
+
 export const registerGet = async (req = request, res = response) => {
     return res.render('register', {styles:'login.css'})
+}
+
+export const registerPost = async (req = request, res = response) => {
+    const {password, confirmPassword} = req.body;
+
+    if(password !== confirmPassword)
+        return res.redirect('/register')
+
+    const user = await registerUser({...req.body})
+
+    if(user){
+        const userName = `${user.name} ${user.lastName}`;
+        req.session.user = userName;
+        req.session.rol = user.rol;
+    }
+
+    return res.redirect('/register')
 }
--- src/routers/initial.js
+++ src/routers/initial.js
@@ -2,9 +2,11 @@ import { Router } from 'express';
 import {cartIdView, 
         chatView, 
         loginGet, 
+        loginPost, 
         productsView, 
         realTimeProductsView,
         registerGet,
+        registerPost,
     } from '../controllers/initial.js';
 
 const router = Router();
@@ -14,11 +16,11 @@ router.get('/real-time-products', realTimeProductsView);
 router.get('/chat', chatView);
 router.get('/cart/:cid', cartIdView);
 
-router.get('/login', loginGet)
-//router.post('/login', )
+router.get('/login', loginGet);
+router.post('/login', loginPost);
 
-router.get('/register', registerGet)
-//router.post('/register', )
+router.get('/register', registerGet);
+router.post('/register', registerPost);
 
 router.get('*', (req, res) => {
     return res.render('404');
