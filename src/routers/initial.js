0335d1c871577e63d00eb98ee47708738ee12126
Santiago Guinazu <santigui2003@gmail.com>
2023-12-05 15:14:53 -0300

    desafio 8

--- src/routers/initial.js
+++ src/routers/initial.js
@@ -10,21 +10,20 @@ import {cartIdView,
         registerGet,
         registerPost,
     } from '../controllers/initial.js';
+import { auth } from '../middleware/auth.js';
 
 const router = Router();
 
 router.get('/', homeView)
-router.get('/products', productsView);
-router.get('/real-time-products', realTimeProductsView);
-router.get('/chat', chatView);
-router.get('/cart/:cid', cartIdView);
+router.get('/products', auth, productsView);
+router.get('/real-time-products', auth, realTimeProductsView);
+router.get('/chat', auth, chatView);
+router.get('/cart/:cid', auth, cartIdView);
 
 router.get('/login', loginGet);
 router.post('/login', loginPost);
-
 router.get('/register', registerGet);
 router.post('/register', registerPost);
-
 router.get('/logout', logOut)
 
 router.get('*', (req, res) => {
