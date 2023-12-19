a5beeb8af2f7c8b16ad1298a9a3d63f7202e87dc
Santiago Guinazu <santigui2003@gmail.com>
2023-12-19 10:24:40 -0300

    cloudinary

--- src/controllers/initial.js
+++ src/controllers/initial.js
@@ -1,55 +1,73 @@
 import { request, response } from "express";
-import { getProductsService } from "../services/products.js";
+import { addProductService, getProductByCodeService, getProductsService } from "../services/products.js";
 import { getCartByIdService } from "../services/carts.js";
+import { cloudinary } from "../config/cloduinary.js";
 
 export const homeView = async (req = request, res = response) => {
     const user = req.session.user;
-    return res.render('home', {user})
+    return res.render('home', { user })
 }
 
 export const realTimeProductsView = async (req = request, res = response) => {
     const user = req.session.user;
-    return res.render('productos-real-time', {user})
+    return res.render('productos-real-time', { user })
 }
 
 export const chatView = async (req = request, res = response) => {
     const user = req.session.user;
-    return res.render('chat', {styles: 'chat.css', user})
+    return res.render('chat', { styles: 'chat.css', user })
 }
 
 export const productsView = async (req = request, res = response) => {
-    const result = await getProductsService({...req.query});
+    const result = await getProductsService({ ...req.query });
     const user = req.session.user;
-    return res.render('productos', {result, user})
+    return res.render('productos', { result, user })
 }
 
 export const addProductView = async (req = request, res = response) => {
     const user = req.session.user;
-    return res.render('addProduct', {user})
+    return res.render('addProduct', { user })
 }
 
 export const addProductViewPost = async (req = request, res = response) => {
-    const user = req.session.user;
-    return res.render('addProduct', {user})
+
+    const { title, description, price, code, stock, category } = req.body;
+
+    if (!title, !description, !price, !code, !stock, !category)
+        return res.status(404).json({ msg: 'Los campos: title, description, price, img, code, stock son obligatorios' })
+
+    const existeCode = await getProductByCodeService(code);
+
+    if (existeCode)
+        return res.status(400).json({ msg: 'El codigo ingresado ya existe en un producto' });
+
+    if (req.file) {
+        const { secure_url } = await cloudinary.uploader.upload(req.file.path);
+        req.body.thumbnails = secure_url;
+    }
+
+    await addProductService({ ...req.body });
+
+    return res.redirect('/products')
 }
 
 export const cartIdView = async (req = request, res = response) => {
-    const {cid} = req.params;
+    const { cid } = req.params;
     const carrito = await getCartByIdService(cid);
     const user = req.session.user;
-    return res.render('cart', {carrito, user});
+    return res.render('cart', { carrito, user });
 }
 
 export const loginGet = async (req = request, res = response) => {
-    if(req.session.user)
+    if (req.session.user)
         return res.redirect('/')
-    return res.render('login', {styles:'login.css'})
+    return res.render('login', { styles: 'login.css' })
 }
 
 export const login = async (req = request, res = response) => {
-    if(!req.user)
+    if (!req.user)
         return res.redirect('/login');
-    
+
     req.session.user = {
         name: req.user.name,
         lastName: req.user.lastName,
@@ -62,22 +80,22 @@ export const login = async (req = request, res = response) => {
 
 export const logOut = (req = request, res = response) => {
     req.session.destroy(err => {
-        if(err)
-            return res.send({status: false, body: err})
+        if (err)
+            return res.send({ status: false, body: err })
         else
             return res.redirect('/login')
-        
+
     })
 }
 
 export const registerGet = async (req = request, res = response) => {
-    if(req.session.user)
+    if (req.session.user)
         return res.redirect('/')
-    return res.render('register', {styles:'login.css'})
+    return res.render('register', { styles: 'login.css' })
 }
 
 export const registerPost = async (req = request, res = response) => {
-    if(!req.user)
+    if (!req.user)
         return res.redirect('/register');
 
     return res.redirect('/login');
--- src/routers/initial.js
+++ src/routers/initial.js
@@ -1,15 +1,5 @@
 import { Router } from 'express';
-import {addProductView, addProductViewPost, cartIdView, 
-        chatView, 
-        homeView, 
-        logOut, 
-        login, 
-        loginGet, 
-        productsView, 
-        realTimeProductsView,
-        registerGet,
-        registerPost,
-    } from '../controllers/initial.js';
+import {addProductView, addProductViewPost, cartIdView, chatView, homeView, logOut, login, loginGet, productsView, realTimeProductsView,registerGet,registerPost,} from '../controllers/initial.js';
 import { admin, auth } from '../middleware/auth.js';
 import passport from 'passport';
 import { uploader } from '../config/multer.js';
@@ -20,22 +10,20 @@ router.get('/', homeView)
 router.get('/products', [auth], productsView);
 router.get('/real-time-products', [auth, admin], realTimeProductsView);
 router.get('/chat', [auth], chatView);
-router.get('/add-product', [auth, admin, uploader.single('file')], addProductViewPost)
-router.post('/add-product', [auth, admin], addProductView)
-router.get('/cart/:cid', [auth, admin], cartIdView);
+router.get('/add-product', [auth], addProductView);
+router.get('/cart/:cid', [auth], cartIdView);
 
 router.get('/login', loginGet);
 router.get('/register', registerGet);
 router.get('/logout', logOut)
 
+router.post('/add-product', [auth, uploader.single('file')], addProductViewPost);
 router.post('/login', passport.authenticate('login',{failureRedirect:'/login'}), login);
 router.post('/register', passport.authenticate('register',{failureRedirect:'/register'}), registerPost);
 
 router.get('/github', passport.authenticate('github', {scope:['user:email']}), async(req,res) => {})
 router.get('/githubcallback', passport.authenticate('github', {failureRedirect:'/register'}), login)
 
-router.get('*', (req, res) => {
-    return res.render('404');
-});
+router.get('*', (req, res) => {return res.render('404')});
 
 export default router;
