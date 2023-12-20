daec52dbbc419abe22708436bee17c5611ffae44
Santiago Guinazu <santigui2003@gmail.com>
2023-12-20 13:47:09 -0300

    cloudinary

--- src/models/user.js
+++ src/models/user.js
@@ -6,7 +6,7 @@ const UserSchema = new Schema({
     name:{type: String, required: [true,'El nombre es obligatorio']},
     lastName:{type: String},
     email:{type: String, required: [true,'El correo es obligatorio'], unique: true},
-    age:{type: Number},
+    age:{type: Number, required: [true,'La edad es obligatoria']},
     cart:{type: String},
     password:{type: String, required: [true,'La contraseña es obligatoria']},
     rol:{type: String, default: 'user', enum: ['user', 'admin']},
