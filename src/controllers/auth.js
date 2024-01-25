45416b4eef56644e48e7606adba0ef7042f2b077
Santiago Guinazu <santigui2003@gmail.com>
2024-01-25 10:53:08 -0300

    tp3

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,11 +1,20 @@
 import { response, request } from 'express';
 import { UsersRepository } from '../repositories/index.js';
-import { createHash } from '../utils/bcryptPassword.js';
+import { createHash, isValidPassword } from '../utils/bcryptPassword.js';
 import { generateToken } from '../utils/jsonWebToken.js';
 
 export const loginUsuario = async(req=request, res=response) => {
     try {
+        const {email, password} = req.body;
+
+        const usuario = await UsersRepository.getUserByEmail(email);
+        if(!usuario) return res.status(400).json({ok:false, msg: 'Datos incorrectos'})
+
+        const validPassword = isValidPassword(password, usuario.password);
+        if(!validPassword) return res.status(400).json({ok:false, msg: 'Datos incorrectos'})
+
         return res.json({ok:true, msg:'loginUsuario'})
+
     } catch (error) {
         console.log(error);
         return res.status(500).json({ok:false, msg: 'Por favor, contactarse con un admin'})
@@ -14,9 +23,11 @@ export const loginUsuario = async(req=request, res=response) => {
 export const crearUsuario = async(req=request, res=response) => {
     try {
         req.body.password = createHash(req.body.password);
+        
         const result = await UsersRepository.registerUser(req.body);
         const {_id, name, lastName, email, rol} = result;
         const token = generateToken({_id, name, lastName, email, rol});
+
         return res.json({ok:true, result, token});
     } catch (error) {
         console.log(error);
