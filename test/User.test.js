1779ae6398003deaec69a7992b5906653dba5824
Santiago Guinazu <santigui2003@gmail.com>
2024-03-01 17:40:26 -0300

    desafio post vacas

---
+++ test/User.test.js
@@ -0,0 +1,20 @@
+import { expect } from 'chai';
+import supertest from 'supertest';
+import { logger } from '../src/utils/logger.js';
+
+const request = supertest('http://127.0.0.1:8080');
+
+describe('Testing Users',()=>{
+    
+    describe('LoginTest', async()=>{
+        const user = {
+            email:'santi@santi.com',
+            password:'123456',
+        }
+        const {statusCode, ok, body} = await request.post('/api/auth/login').send(user);
+        logger.info(statusCode)
+        logger.info(ok)
+        logger.info(JSON.stringify(body))
+    });
+})
+
