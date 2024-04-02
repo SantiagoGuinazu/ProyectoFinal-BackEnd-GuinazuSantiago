a25b7b72c8300210fbe1ac7c609efafdec89a6da
Santiago Guinazu <santigui2003@gmail.com>
2024-04-02 11:28:30 -0300

    back

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import swaggerJsDoc from 'swagger-jsdoc';
 import swaggerUiExpress from 'swagger-ui-express';
 
-import { productsRouter, cartsRouter, authRouter, ticketsRouter, paymentRouter } from './routers/index.js';
+import { productsRouter, cartsRouter, authRouter, ticketsRouter } from './routers/index.js';
 
 import __dirname from './utils.js';
 import { dbConnection } from './database/config.js';
@@ -36,7 +36,6 @@ app.use('/api/auth', authRouter);
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
 app.use('/api/tickets', ticketsRouter);
-app.use('/api/payments', paymentRouter);
 app.use('/documentacion-api', swaggerUiExpress.serve, swaggerUiExpress.setup(spec));
 
 await dbConnection();
--- src/routers/index.js
+++ src/routers/index.js
@@ -2,4 +2,3 @@ export * from './products.js';
 export * from './carts.js';
 export * from './auth.js';
 export * from './tickets.js';
-export * from './payments.js';
