f8c79c188591c3d5c7223fb19ac43881fd0cfb7d
Santiago Guinazu <santigui2003@gmail.com>
2024-04-02 12:25:02 -0300

    back

--- src/dao/mongo/cartsMongo.js
+++ src/dao/mongo/cartsMongo.js
@@ -23,14 +23,14 @@ export const addProductCart = async (cid, pid) => {
 }
 
 export const deleteProductsInCart = async (cid, pid) => 
-    await cartModel.findByIdAndUpdate(cid, { $pull: { 'products': { id: pid } } }, { new: true }).populate('products.id', ['title', 'price', 'stock','thumbnails']);
+    await cartModel.findByIdAndUpdate(cid, { $pull: { 'products': { id: pid } } }, { new: true }).populate('products.id', ['title', 'price', 'stock','thumbnail']);
 
 export const updateProductsInCart = async (cid, pid, quantity) => {
     return await cartModel.findOneAndUpdate(
         { _cid: cid, 'products.id': pid },
         { $set: { 'products.$.quantity': quantity } },
         { new: true }
-    ).populate('products.id', ['title', 'price', 'stock','thumbnails']);
+    ).populate('products.id', ['title', 'price', 'stock','thumbnail']);
 };
 
 export const deleteCart = async (cid) => await cartModel.findByIdAndDelete(cid);
