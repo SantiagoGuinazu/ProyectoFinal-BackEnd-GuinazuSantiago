8a98e0861b4a18345cdc40a8195ec0feae4ff2b9
Santiago Guinazu <santigui2003@gmail.com>
2024-04-06 11:38:36 -0300

    back

--- src/controllers/carts.js
+++ src/controllers/carts.js
@@ -156,12 +156,12 @@ export const createIdPreference = async (req = request, res = response) => {
         });
 
         const back_urls = {
-            success: "https://guinazusantiago-ecommerce-front.netlify.app/mi-carrito",
-            failure: "https://guinazusantiago-ecommerce-front.netlify.app/mi-carrito",
-            pending: "https://guinazusantiago-ecommerce-front.netlify.app/mi-carrito"
-            //success: "http://localhost:5173/mi-carrito",
-            //failure: "http://localhost:5173/mi-carrito",
-            //pending: "http://localhost:5173/mi-carrito"
+            //success: "https://guinazusantiago-ecommerce-front.netlify.app/mi-carrito",
+            //failure: "https://guinazusantiago-ecommerce-front.netlify.app/mi-carrito",
+            //pending: "https://guinazusantiago-ecommerce-front.netlify.app/mi-carrito"
+            success: "http://localhost:5173/mi-carrito",
+            failure: "http://localhost:5173/mi-carrito",
+            pending: "http://localhost:5173/mi-carrito"
         };
 
         const body = {
