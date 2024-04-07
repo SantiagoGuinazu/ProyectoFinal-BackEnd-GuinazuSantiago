46b3d10e2758849f4f01c17fa9e6b0deb63f8001
Santiago Guinazu <santigui2003@gmail.com>
2024-04-07 16:51:37 -0300

    back

--- src/repositories/messagesRepository.js
+++ src/repositories/messagesRepository.js
@@ -1,3 +1,3 @@
 import {MessageDao} from "../dao/index.js";
 
-export const getMessages = async (user, message) => await MessageDao.getMessages(user, message);
+export const getMessages = async (user, message) => await MessageDao.getMessa(user, message);
